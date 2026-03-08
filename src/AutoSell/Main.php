f713fe1651bd175ff9ca35871d8867160a0f1743
ZyberPE <zyberpe@gmail.com>
2026-03-08 00:16:51 -0500

    Create Main.php

---
+++ src/AutoSell/Main.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AutoSell;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\player\Player;
+
+use pocketmine\utils\TextFormat;
+
+use onebone\economyapi\EconomyAPI;
+
+class Main extends PluginBase implements Listener {
+
+    private array $enabled = [];
+
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+    /* -------------------------
+       COMMAND
+    --------------------------*/
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+
+        if(!$sender instanceof Player){
+            return true;
+        }
+
+        $msg = $this->getConfig()->get("messages");
+
+        if(!$sender->hasPermission("autosell.use")){
+            $sender->sendMessage($msg["no-permission"]);
+            return true;
+        }
+
+        if(!isset($args[0])){
+            $sender->sendMessage($msg["usage"]);
+            return true;
+        }
+
+        switch(strtolower($args[0])){
+
+            case "on":
+                $this->enabled[$sender->getName()] = true;
+                $sender->sendMessage($msg["enabled"]);
+            break;
+
+            case "off":
+                unset($this->enabled[$sender->getName()]);
+                $sender->sendMessage($msg["disabled"]);
+            break;
+
+            default:
+                $sender->sendMessage($msg["usage"]);
+            break;
+        }
+
+        return true;
+    }
+
+    /* -------------------------
+       BLOCK SELL
+    --------------------------*/
+
+    public function onBreak(BlockBreakEvent $event): void {
+
+        $player = $event->getPlayer();
+
+        if(!isset($this->enabled[$player->getName()])){
+            return;
+        }
+
+        $block = $event->getBlock();
+        $name = strtolower($block->getName());
+
+        $prices = $this->getConfig()->get("sell-prices");
+
+        if(!isset($prices[$name])){
+            return;
+        }
+
+        $price = (float)$prices[$name];
+
+        EconomyAPI::getInstance()->addMoney($player,$price);
+
+        $subtitle = str_replace(
+            ["{block}","{amount}"],
+            [$block->getName(),$price],
+            $this->getConfig()->get("subtitle-format")
+        );
+
+        $player->sendTitle("",$subtitle,0,20,0);
+
+        $event->setDrops([]); // remove item drop
+    }
+}
